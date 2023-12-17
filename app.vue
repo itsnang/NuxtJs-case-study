208c64112cbbdcb1346da46ed0044ccb4b69f764
Samnang Lorn <lorn.samnang@cellcard.com.kh>
2023-12-17 15:19:21 +0700

    feat: apply tailwindcss

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="text-red-400">
-    <Button>Click me</Button>
-  </div>
-</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        laptop: '90rem',
+      },
+    },
   },
   plugins: [],
 }
