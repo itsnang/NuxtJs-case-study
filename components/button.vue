bf58a83901746f7bd757193f54a6b869993f3924
Samnang Lorn <lorn.samnang@cellcard.com.kh>
2023-12-18 11:39:58 +0700

    feat: reused button

--- components/button.vue
+++ components/button.vue
@@ -1,12 +1,19 @@
-
 <template>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    <slot></slot>
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    <NuxtLink v-if="isLink == true" :to="href">
+      <slot></slot>
+    </NuxtLink>
   </button>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Button',
-}
+  name: "Button",
+  props: {
+    isLink: Boolean,
+    href: String,
+  },
+};
 </script>
