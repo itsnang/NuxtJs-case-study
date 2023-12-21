77d073f9abf1c0ffc818f4fa9e366c97a4c2a28c
itsnang <lorn.samnang.1221@rupp.edu.kh>
2023-12-21 16:06:18 +0700

    Refactor header and add footer

--- components/header.vue
+++ components/header.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="flex justify-between bg-gray-100 py-4  px-10">
-    <NuxtLink to="/"
-      class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text text-2xl">
-      NuxtJS
+  <div class="flex items-center justify-between bg-gray-100 py-4 px-10">
+    <NuxtLink
+      to="/"
+      class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text font-bold text-2xl"
+    >
+      Fake Store
     </NuxtLink>
     <div class="gap-6 flex">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
       <NuxtLink to="/products">Products</NuxtLink>
-
     </div>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Header',
-}
+  name: "Header",
+};
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,8 @@
   <Header />
   <div class="max-w-laptop mx-auto mb-8 min-h-screen lg:px-10 md:px-8 px-4">
     <slot />
+    <footer class="bg-blue-300 p-4 text-white text-center">
+      <p>&copy; 2023 My E-Commerce Store. All rights reserved.</p>
+    </footer>
   </div>
 </template>
